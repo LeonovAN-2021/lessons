b09d66a51669cee93c4f25fd3f8b44f4a5f809b0
leonov_an <leonard@lenta.ru>
2021-02-13 22:07:47 +0400

    first lesson01 commit

---
+++ TestProg-1/TestProg-1/Lesson1/MainClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace TestProg_1.Lesson1
+{
+    class MainClass
+    {
+        static void Main(string[] args) 
+        {
+            string username="";
+            DateTime datenow= DateTime.Now ;
+            string weekday = CultureInfo.CurrentCulture.DateTimeFormat.GetDayName(datenow.DayOfWeek);
+
+
+            Console.Write("Привет, как тебя зовут? ");
+            username=Console.ReadLine();
+            Console.WriteLine("Привет, "+username+"! Сегодня "+weekday+", {0:F}", datenow);
+        }
+
+            
+    }
+    
+}
