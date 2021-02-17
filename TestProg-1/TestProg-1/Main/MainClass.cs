c048fb2ad6985cc52a8871cde9e3e820f5ab5eb1
leonov_an <leonard@lenta.ru>
2021-02-17 13:22:38 +0400

    Lesson 2 added

---
+++ TestProg-1/TestProg-1/Main/MainClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace TestProg_1.Main
+{
+   
+    class MainClass
+    {
+        
+        static void Main(string[] args) 
+        {
+            int LessonNum = 1;
+            Console.Write("Номер урока: ");
+            LessonNum=Convert.ToInt32(Console.ReadLine());
+         
+            switch (LessonNum)
+            {
+                case 1:
+                    Console.WriteLine("Задача 1. Приветствие");
+                    Lesson1.Less01.Les();
+                    break;
+                case 2:
+                    
+                    Lesson2.Less02.Les();
+                    
+                   
+                    
+                    
+                    
+                    break;
+            }
+        }
+        
+
+
+    }
+    
+}
