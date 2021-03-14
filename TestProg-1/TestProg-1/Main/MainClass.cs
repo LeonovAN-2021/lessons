03db2e528b58b17ef0f1d594822531a50c37449f
leonov_an <leonard@lenta.ru>
2021-03-14 15:04:05 +0400

    files for 7 lesson

--- TestProg-1/TestProg-1/Main/MainClass.cs
+++ TestProg-1/TestProg-1/Main/MainClass.cs
@@ -9,29 +9,12 @@ namespace TestProg_1.Main
     class MainClass
     {
         
-        static void Main(string[] args) 
+        static void Main(string[] args)
         {
-            int LessonNum = 1;
-            Console.Write("Номер урока: ");
-            LessonNum=Convert.ToInt32(Console.ReadLine());
-         
-            switch (LessonNum)
-            {
-                case 1:
-                    Console.WriteLine("Задача 1. Приветствие");
-                    Lesson1.Less01.Les();
-                    break;
-                case 2:
-                    
-                    Lesson2.Less02.Les();
-                    
-                   
-                    
-                    
-                    
-                    break;
-            }
+            //Battleships.btl.test();
+            Cross.Cross.Game();
         }
+            
         
 
 
